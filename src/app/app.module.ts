e394587120db461d792876d89e64969286e2dc43
cdquinterop <darmiqp@gmail.com>
2023-04-30 00:14:37 -0500

    Functionality to make the connection to the api and process the image, as well as the functionality to download the content.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageConverterComponent } from './image-converter/image-converter.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ImageConverterComponent } from './image-converter/image-converter.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
